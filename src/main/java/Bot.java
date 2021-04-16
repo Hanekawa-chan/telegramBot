177310b7135f0998e29afb993b814af28e65dc6e
Hanekawa-chan <captainduck.official@gmail.com>
2021-04-16 20:55:02 +0300

    make it better

---
+++ src/main/java/Bot.java
@@ -0,0 +1,55 @@
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@NoArgsConstructor
+public class Bot extends TelegramLongPollingBot {
+
+    /**
+     * Метод для приема сообщений.
+     * @param update Содержит сообщение от пользователя.
+     */
+    @Override
+    public void onUpdateReceived(Update update) {
+        try {
+            //проверяем есть ли сообщение и текстовое ли оно
+            if (update.hasMessage() && update.getMessage().hasText()) {
+                //Извлекаем объект входящего сообщения
+                Message inMessage = update.getMessage();
+                //Создаем исходящее сообщение
+                SendMessage outMessage = new SendMessage();
+                //Указываем в какой чат будем отправлять сообщение
+                //(в тот же чат, откуда пришло входящее сообщение)
+                outMessage.setChatId(String.valueOf(inMessage.getChatId()));
+                //Указываем текст сообщения
+                outMessage.setText(inMessage.getText());
+                //Отправляем сообщение
+                execute(outMessage);
+            }
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Метод возвращает имя бота, указанное при регистрации.
+     * @return имя бота
+     */
+    @Override
+    public String getBotUsername() {
+        return "testchubot";
+    }
+
+    /**
+     * Метод возвращает token бота для связи с сервером Telegram
+     * @return token для бота
+     */
+    @Override
+    public String getBotToken() {
+        return "1727229817:AAHTHiuW4qu7SFgZw59M7ZATkQ6FXTVrIqQ";
+    }
+}
