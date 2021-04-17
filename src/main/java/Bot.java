b6c74c61d6a113a4232ca8daf2bfa9712ec32cde
Hanekawa-chan <captainduck.official@gmail.com>
2021-04-17 19:53:29 +0300

    pushing

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -6,6 +6,8 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.util.Date;
+
 @NoArgsConstructor
 @Getter
 @Setter
@@ -14,38 +16,49 @@ public class Bot extends TelegramLongPollingBot {
     private boolean isOn = false;
     private boolean isAuthed = false;
 
-    @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
+
+            String messageText = update.getMessage().getText();
+            String id = String.valueOf(update.getMessage().getChatId());
+
             if (isOn) {
-                if(update.getMessage().getText().equals("password")) {
-                    SendMessage message = new SendMessage();
-                    message.setChatId(String.valueOf(update.getMessage().getChatId()));
-                    message.setText("Правильный пароль :)");
-                    execute(message);
+                if(messageText.equals("password")) {
+                    send(id, "Правильный пароль :)");
                     isAuthed = true;
                     isOn = false;
                 }
                 else {
-                    SendMessage message = new SendMessage();
-                    message.setChatId(String.valueOf(update.getMessage().getChatId()));
-                    message.setText("Неправильный пароль :(");
-                    execute(message);
+                    send(id,"Неправильный пароль :(");
                     isOn = false;
                 }
             }
-            if (update.getMessage().getText().equals("/hi")) {
-                SendMessage message = new SendMessage();
-                message.setChatId(String.valueOf(update.getMessage().getChatId()));
-                message.setText("Привет, " + update.getMessage().getFrom().getUserName() + "\n" +
-                         "Пожалуйста, введите пароль");
-                execute(message);
+            if (messageText.equals("/hi") && !isAuthed) {
+                send(id,"Привет, " + update.getMessage().getFrom().getUserName()
+                        + "\n" +
+                        "Пожалуйста, введите пароль");
                 isOn = true;
+            }
+            if (isAuthed && messageText.equals("/start")) {
+                while(true) {
+                    Date date = new Date();
+                    System.out.println(date + " Время=" + date.toString().subSequence(11,18));
+                    if(date.toString().subSequence(11,18).equals("20:00:00")) {
+                        send(id, "Время настало!");
+                    }
+                }
             }
         }
     }
 
+    @SneakyThrows
+    private void send(String id, String text) {
+        SendMessage message = new SendMessage();
+        message.setChatId(id);
+        message.setText(text);
+        execute(message);
+    }
     /**
      * Метод возвращает имя бота, указанное при регистрации.
      * @return имя бота
